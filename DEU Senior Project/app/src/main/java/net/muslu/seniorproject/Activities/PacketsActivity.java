9015b1250df2d26aa1d4a07d6be0f81156d9c157
Tayyip MUSLU <muhammet.muslu@ceng.deu.edu.tr>
2019-12-16 23:41:39 +0300

    packets activity: customer calling and packet deleted..

--- DEU Senior Project/app/src/main/java/net/muslu/seniorproject/Activities/PacketsActivity.java
+++ DEU Senior Project/app/src/main/java/net/muslu/seniorproject/Activities/PacketsActivity.java
@@ -1,19 +1,24 @@
 package net.muslu.seniorproject.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import net.muslu.seniorproject.CustomAdapter;
@@ -22,12 +27,14 @@ import net.muslu.seniorproject.ProjectData;
 import net.muslu.seniorproject.R;
 import net.muslu.seniorproject.Reader.Barcode.BarcodeData;
 import net.muslu.seniorproject.Reader.Barcode.BarcodeRead;
+import net.muslu.seniorproject.Reader.Barcode.BarcodeReadModel;
 
 public class PacketsActivity extends AppCompatActivity {
 
     private BarcodeData data;
     private RecyclerView rv;
     private CustomAdapter ad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +45,51 @@ public class PacketsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        if(intent.getExtras() != null){
+        if (intent.getExtras() != null) {
             DataTransfer dataTransfer = (DataTransfer) intent.getExtras().getSerializable("data");
             setData(dataTransfer.getBarcodeData());
             rv = findViewById(R.id.rv);
-            ad = new CustomAdapter(PacketsActivity.this, getData().GetData());
+            ad = new CustomAdapter(PacketsActivity.this, getData().GetData(), new CustomAdapter.ClickListener() {
+
+                @Override
+                public void onPositionClicked(final View view,final BarcodeReadModel model, final int pos) {
+                    if (view.getId() == R.id.makeCall) {
+                        Toast.makeText(view.getContext(), "Müşteri aranıyor..", Toast.LENGTH_SHORT).show();
+                        Uri uri = Uri.parse("tel:" + model.getCustomer().getPhone());
+
+                        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                        if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                                Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions(PacketsActivity.this,
+                                    new String []{Manifest.permission.CALL_PHONE},1);
+                        }
+                        startActivity(intent);
+                    }
+                    else if (view.getId() == R.id.delete) {
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(PacketsActivity.this);
+                        builder.setTitle(getString(R.string.packet_delete_title));
+                        builder.setMessage(model.getCustomer().getFullName() + " " + getString(R.string.packet_delete_desc));
+                        builder.setNegativeButton(getString(R.string.no), null);
+                        builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                Toast.makeText(view.getContext(), model.getCustomer().getFullName() + " müşterisinin " + model.getBarcode() + " numaralı paketi kaldırıldı." , Toast.LENGTH_SHORT).show();
+                                data.GetData().remove(pos);
+                                rv.setAdapter(ad);
+
+                            }
+                        });
+                        builder.show();
+
+
+
+                    } else {
+                        Toast.makeText(view.getContext(), "ROW PRESSED = " , Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
             rv.setAdapter(ad);
             rv.setLayoutManager(new LinearLayoutManager(this));
             rv.setHasFixedSize(false);
--- DEU Senior Project/app/src/main/java/net/muslu/seniorproject/CustomAdapter.java
+++ DEU Senior Project/app/src/main/java/net/muslu/seniorproject/CustomAdapter.java
@@ -2,45 +2,68 @@ package net.muslu.seniorproject;
 
 import android.content.Context;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-
-import androidx.annotation.NonNull;
+import android.widget.Toast;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.squareup.picasso.Picasso;
-
 import net.muslu.seniorproject.Reader.Barcode.BarcodeReadModel;
-
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
     private Context mContext;
-    private ArrayList<BarcodeReadModel> data;
+    static private ArrayList<BarcodeReadModel> data;
+    private final ClickListener listener;
+    private static WeakReference<ClickListener> listenerRef;
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public interface ClickListener {
+
+        void onPositionClicked(View view, BarcodeReadModel model, int pos);
+
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
 
         TextView customerName;
         TextView customerAddress;
-        ImageView packageBarcode;
+        ImageView packageBarcode, phone, remove;
 
 
-        public MyViewHolder(View itemView) {
+        public MyViewHolder(View itemView, ClickListener listener) {
             super(itemView);
-
+            listenerRef = new WeakReference<>(listener);
             this.customerAddress = (TextView) itemView.findViewById(R.id.CustomerAddress);
             this.customerName = (TextView) itemView.findViewById(R.id.CustomerName);
             this.packageBarcode = (ImageView) itemView.findViewById(R.id.PackageBarcode);
+            this.phone = itemView.findViewById(R.id.makeCall);
+            this.remove = itemView.findViewById(R.id.delete);
+
+            phone.setOnClickListener(this);
+            remove.setOnClickListener(this);
+            //itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            listenerRef.get().onPositionClicked(v, data.get(getAdapterPosition()),getAdapterPosition());
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            return false;
         }
     }
 
-    public CustomAdapter(Context mContext, ArrayList<BarcodeReadModel> data) {
+    public CustomAdapter(Context mContext, ArrayList<BarcodeReadModel> data, ClickListener listener) {
         this.mContext = mContext;
         this.data = data;
+        this.listener = listener;
     }
 
     @Override
@@ -48,7 +71,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
                                            int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.read_barcode_view, parent, false);
 
-        MyViewHolder myViewHolder = new MyViewHolder(view);
+        MyViewHolder myViewHolder = new MyViewHolder(view, listener);
         return myViewHolder;
     }
 
